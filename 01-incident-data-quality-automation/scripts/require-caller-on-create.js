438ac7b2573490c4df57fc5b74a4af270e1594d8
lonestarmohammed786 <mohdnadeemuddin01@gmail.com>
2026-06-09 05:04:21 -0500

    Require caller_id before creating an incident
    Added a business rule to ensure caller_id is provided.

--- 01-incident-data-quality-automation/scripts/require-caller-on-create.js
+++ 01-incident-data-quality-automation/scripts/require-caller-on-create.js
@@ -1,1 +1,11 @@
+/*
+Project: Incident Data Quality Automation
+Type: Before Insert Business Rule
+Table: Incident [incident]
+Purpose: Blocks Incident creation if caller_id is empty.
+*/
 
+if (gs.nil(current.caller_id)) {
+    gs.addErrorMessage('Caller is required before creating an incident.');
+    current.setAbortAction(true);
+}
